39038fd034c15f09b513cf6bc135a7f5c5350db3
Alexey Bykovskiy <bykovskijay@answer-42.ru>
2023-10-08 12:37:59 +0700

    Update localization work

--- src/common/i18n/i18n.ts
+++ src/common/i18n/i18n.ts
@@ -1,7 +1,13 @@
 import { I18n } from 'i18n'
 import path from 'path'
 
-import { Locales } from '@/types'
+import { Locales, Paths } from '@/types'
+
+import { Log } from '../logger'
+
+import ruLocale from './locales/ru.json'
+
+type Locale = typeof ruLocale
 
 export const i18n = new I18n({
   defaultLocale: Locales.Ru,
@@ -11,10 +17,14 @@ export const i18n = new I18n({
   missingKeyFn: (locale, key) => {
     const message = `🌏i18next: Missing translation for ${key} in locale: ${locale}`
 
-    console.log(message)
+    Log.warn(message)
 
     return message
   },
 })
 
-export const { __mf: t } = i18n
+export const { __mf } = i18n
+export type Phrase = Paths<Locale>
+
+export const t = (phrase: { phrase: Phrase; locale: Locales }, replace?: Record<string, unknown>) =>
+  __mf(phrase, replace)
